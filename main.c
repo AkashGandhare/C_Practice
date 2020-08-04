5efaf5da0055a27fc2dbc0a144e82481a857d865
akashGandhare <akash.gandhare@gmail.com>
2020-08-04 19:57:50 +0200

    struct concept

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+extern void test_unsigned_int();
+extern void struct_concept();
 int main()
 {
     //test_pointer(); /**< assigning char* to int* and test */
-    forLoop_test(); /**< for loop testing with multiple conditions */
+//    forLoop_test(); /**< for loop testing with multiple conditions */
+//    test_unsigned_int();
+    struct_concept();
 
     return 0;
 }
