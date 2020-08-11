1fe4eecb87ea260ccae3069cf675c45b26a10f13
akashGandhare <akash.gandhare@gmail.com>
2020-08-11 19:28:07 +0200

    static recursive function concept

--- main.c
+++ main.c
@@ -3,8 +3,11 @@
 extern void test_unsigned_int();
 extern void struct_concept();
 extern void recursive(int);
+extern void float_modulo(float);
+extern void  static_recursive();
 int main()
 {
+
 //      test_pointer(); /**< assigning char* to int* and test */
 //      forLoop_test(); /**< for loop testing with multiple conditions */
 //      test_unsigned_int();
@@ -12,7 +15,10 @@ int main()
 //      test_pointer_2();
 //        test_uninitialized_var();
 //        struct_concept_2();
-        recursive(20);
+//        recursive(20);
+//        float_modulo(20.0);
+        static_recursive();
+
 
     return 0;
 }
